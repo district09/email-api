30bcab463ce0a2828a559504d1fb68252c8b3f9c
Ghijs Matthias <Matthias.Ghijs@district09.gent>
2021-10-04 16:27:05 +0200

    Fix: fixed karate api version

--- karate/src/test/resources/karate-config-dv.js
+++ karate/src/test/resources/karate-config-dv.js
@@ -2,7 +2,7 @@ function fn() {
     var env = karate.env; // get java system property 'karate.env'
     karate.log('karate.env system property was:', env);
     var config = { // base config JSON
-      baseUrl: 'http://email-api.servicefactorydv.svc:8080/supporting/email/v3',
+      baseUrl: 'http://email-api-v3.servicefactorydv.svc:8080/supporting/email/v3',
     };
     karate.configure('connectTimeout', 60000);
     karate.configure('readTimeout', 60000);
